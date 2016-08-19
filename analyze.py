1890ee2d03c668d975958439d8740c48a09bee70
Matt Kramer <matt314159@gmail.com>
2016-08-18 17:37:56 -0700

    Add remaining templates

--- analyze.py
+++ analyze.py
@@ -46,6 +46,42 @@ def gen_spe_2p2ft_templates():
         for chan in range(1, 8):
             dirname = glob('RawData/SinglePhotoelectron/%s%d_2pt2*_5mV' % (side, chan))[0]
             gph = get_template(dirname)
-            gph.SetName('%s%d' % (side, chan))
+            gph.SetName('g_spe_%s%d' % (side, chan))
             gph.SetTitle('%s%d, 2.2 ft cable, -5 mV trigger' % (side, chan))
+            gph.Write()
+
+def gen_cosmic_templates():
+    outf = R.TFile('Templates/Cosmics_2.2ft_15mV.root', 'RECREATE')
+
+    for side in ['R', 'L']:
+        for chan in [6, 7]:
+            dirname = glob('RawData/CosmicRays/%s%d_2pt2*_15mV' % (side, chan))[0]
+            gph = get_template(dirname)
+            gph.SetName('g_cosmic_%s%d' % (side, chan))
+            gph.SetTitle('%s%d, 2.2 ft cable, -15 mV trigger (cosmics)' % (side, chan))
+            gph.Write()
+
+def gen_scint_templates():
+    outf = R.TFile('Templates/ScintCosmics_2.2ft_15mV.root', 'RECREATE')
+
+    for side in ['R', 'L']:
+        for chan in [6, 7]:
+            dirname = glob('RawData/WithScintilator/%s%d_2pt2*_15mV' % (side, chan))[0]
+            gph = get_template(dirname)
+            gph.SetName('g_scint_%s%d' % (side, chan))
+            gph.SetTitle('%s%d, 2.2 ft cable, -15 mV trigger (cosmics), w/ scintillator' % (side, chan))
+            gph.Write()
+
+def gen_length_templates():
+    outf = R.TFile('Templates/DifferentLengths_5mV.root', 'RECREATE')
+
+    for cid in ['R3', 'R4', 'L4']:
+        for length in ['2pt2', '12pt5', '25', '50']:
+            if cid == 'R4' and length == '12pt5':
+                continue        # last measurement, board went haywire
+
+            dirname = glob('RawData/DifferentLengths/%s/%s_%s*_5mV' % (cid, cid, length))[0]
+            gph = get_template(dirname)
+            gph.SetName('g_%sft_%s' % (length, cid))
+            gph.SetTitle('%s, %s ft cable, -5 mV trigger' % (cid, length))
             gph.Write()
